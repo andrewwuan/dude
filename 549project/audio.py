20e4275c9ebe6d32f21ececed774e5894eafd46d
hingon <hmiu@andrew.cmu.edu>
2015-03-12 18:03:47 -0400

    audio.py

--- 549project/audio.py
+++ 549project/audio.py
@@ -1,13 +1,44 @@
 #!/usr/bin/python
 
+import subprocess
 
+database = []
+target = ""
 
 def insert_audio(name, audio):
+    # [audio] is the address of sound file by user [name]
+    # 1. change [audio] to a new address to avoid overwrite
+    new_addr = "%.flac" % (name)
+    subprocess.call("mv file.flac %" % (new_addr))
+    # 2. store data to database
+    database.append((name, new_addr))
 
-    return
+def correlate(audio1, audio2):
 
+    return True
+
+def match(elem):
+    name = elem[0]
+    addr = elem[1]
+     
+    if (correlate(target, addr)):
+        return name
+    else:
+        return ""
+
+def find(elem):
+    return (elem != "")
 
 def check_audio(audio):
-    name = ""
+    if (database == []):
+        return ""
+    
+    global target 
+    target = audio
+    matches = map(match, database)
+    result = filter(find, matches)
+    if (result == []):
+        return ""
+    else:
+        return result[0]
 
-    return name
