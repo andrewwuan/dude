1783c1c3df49302e57bf091ac39c62ef9c170d6b
HINGON <miuhingon@gmail.com>
2015-03-13 17:59:28 -0400

    g

--- 549project/speech2text_short.sh
+++ 549project/speech2text_short.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #echo "Recording..."
-arecord -D "plughw:1,0" -d 3 -q -f cd -t wav -r 16000 dude.wav
+arecord -D "plughw:1,0" -d 3 -q -f cd -t wav -r 16000 -c 1 dude.wav
 flac dude.wav -f --best --sample-rate 16000 -s -o dude.flac
 #echo "Uploading to Google..."
 wget -q -U "Mozilla/5.0" --post-file dude.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v2/recognize?lang=en-us&client=chromium&key=AIzaSyCNtR-Gkh4uhob8J1oxy6MtpyNn_FNs7vg" | cut -d\" -f8 > stt.txt
