a86b74ee64910b847c71fe1c9c143f16961f8c59
HINGON <miuhingon@gmail.com>
2015-03-13 20:48:41 -0400

    comment out audio code in main.py

--- 549project/main.py
+++ 549project/main.py
@@ -4,7 +4,7 @@ import sys
 import subprocess
 from weather import *
 from date import *
-from audio import *
+#from audio import *
 
 user = ""
 
@@ -21,7 +21,7 @@ while (True):
             print "Just heard %s" % (noise)
             keyword = noise.split()
         
-    user = check_audio("dude.wav")
+    #user = check_audio("dude.wav")
 
     subprocess.call(["./text2speech.sh", 
         "%s what can I do for you" % (user)])
@@ -47,7 +47,7 @@ while (True):
         "name" == request[1] and
         "is" == request[2]):
         user = request[3]
-        insert_audio(user, "dude.wav")
+        #insert_audio(user, "dude.wav")
         response = "hello %s" % (user)
 
     # Check weather
