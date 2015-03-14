16cc1fc38308981d673d863bc2b52dc95747c666
hingon <pi@raspberrypi.(none)>
2015-03-14 22:21:16 +0000

    fix wiki bug

--- 549project/main.py
+++ 549project/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import string
 import subprocess
 from weather import *
 from date import *
@@ -25,7 +26,7 @@ while (True):
     #user = check_audio("dude.wav")
 
     subprocess.call(["./text2speech.sh", 
-        "%s what can I do for you" % (user)])
+        "%s, what can I do for you" % (user)])
 
     # Listen to user's question
     subprocess.call("./speech2text_long.sh")
@@ -41,7 +42,7 @@ while (True):
     # Process request
     request = line.split()
 
-    response = "%s I do not know what to say" % (user)
+    response = "%s, I do not know what to say" % (user)
 
     # Check authencity
     if ("my" == request[0] and 
@@ -49,22 +50,29 @@ while (True):
         "is" == request[2]):
         user = request[3]
         #insert_audio(user, "dude.wav")
-        response = "hello %s" % (user)
+        response = "hello, %s" % (user)
 
     # Check weather
     if ("weather" in request or
         "temperature" in request):
-        response = "%s %s" % (user, check_weather(request))
+        response = "%s, %s" % (user, check_weather(request))
 
     # Check time
     if ("time" in request or
         "date" in request):
-        response = "%s %s" % (user, check_date(request))
+        response = "%s, %s" % (user, check_date(request))
     
     # Check Wiki
     if ("what" == request[0] and
         "is" == request[1]):
-        response = "%s %s" % (user, check_wiki(request))
-    	
+        response = "%s, %s" % (user, check_wiki(request))
+    
+    # cannot handle too long strings
+    if (len(response) > 100):
+        response = response[0:101]
+        last_space = string.rfind(response, " ") 
+        response = response[0:last_space]
+    
+    print response    	
     subprocess.call(["./text2speech.sh", response])
 
--- 549project/wiki.py
+++ 549project/wiki.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 
+import string
 import wikipedia
 
 def check_wiki(request):
     request.pop(0)
     request.pop(0)
     question = string.join(request)                     
-    answer = wikipedia.summary(question, sentences=2)
+    answer = wikipedia.summary(question, sentences=1)
+    start = string.find(answer, "(")
+    end = string.find(answer, ")")    
+    answer = string.replace(answer, answer[start-1:end+1], "")
     return answer
+
