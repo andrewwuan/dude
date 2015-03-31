d3171e893fd64d613c3bd1f94d4cba1c78a52518
Jing Huang <jinghuang@Jings-MacBook-Air.local>
2015-03-31 15:47:43 -0400

    server client

--- 549project/client.py
+++ 549project/client.py
@@ -1,13 +1,41 @@
 #!/usr/bin/python
+import tornado.ioloop
+import tornado.concurrent
+from tornado.httpclient import HTTPClient
+from tornado.httputil import url_concat
+import requests
 
+def synchronous_fetch(url, user, name):
+    http_client = HTTPClient()
+    params = {"user": user, "name": name}
+    url = url_concat(url, params)
+    response = http_client.fetch(url)
+    return response.body
 
+def synchronous_update(url, user, name, value):
+    http_client = HTTPClient()
+    params = {"user": user, "name": name, "value":value}
+    url = url_concat(url, params)
+    response = http_client.fetch(url)
+    return response.body
+
+def synchronous_upload(url, filename):
+    files = {'file': open(filename, 'rb'), }
+    response = requests.post(url, files=files)
+    return response.text
+
+synchronous_upload("http://localhost:8888", 'hello.txt')
+
+synchronous_update("http://localhost:8888", "Amy", "hobby", "reading")
+
+print synchronous_fetch("http://localhost:8888", "Amy", "hobby")
+
+# returns response text
 def check_server(request):
-    # Send request to server
-
-    
-    # Receive response from server
-    response = ""
+    response = synchronous_fetch(request)
     return response
 
-##### Tests ####
-print check_server("why is the sea blue?")
+def update_server(request):
+    response = synchronous_update(request)
+    return response
+
--- 549project/server.py
+++ 549project/server.py
@@ -4,3 +4,65 @@
 ########### TODO #############
 # 1. build a port to listen for connection
 # 2. no matter what request client sends over, server sends back "hello world"
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import torndb
+import MySQLdb
+import unicodedata
+
+from tornado.options import define, options
+
+define("port", default=8888, help="run on the given port", type=int)
+define("mysql_host", default="127.0.0.1:3306", help="dude database host")
+define("mysql_database", default="dude", help="dude database name")
+define("mysql_user", default="dude", help="dude database user")
+define("mysql_password", default="dude", help="dude database password")
+
+class Application(tornado.web.Application):
+	def __init__(self):
+		handlers = [
+			(r"/", MainHandler),
+		]
+		tornado.web.Application.__init__(self, handlers)
+
+		# Have one global connection to the blog DB across all handlers
+		self.db = torndb.Connection(
+			host=options.mysql_host, database=options.mysql_database,
+			user=options.mysql_user, password=options.mysql_password)
+
+
+class MainHandler(tornado.web.RequestHandler):
+	def get(self):
+		user = self.get_argument("user")
+		name = self.get_argument("name")
+		value = self.get_argument("value", None)
+		if (not value):
+			entry = self.application.db.get("SELECT * FROM entries WHERE user = %s AND name = %s",
+				user, name)
+			self.write(entry.value)
+		else:
+			entry = self.application.db.get("SELECT * FROM entries WHERE user = %s", user)
+			if not entry:
+				self.application.db.execute(
+				"INSERT INTO entries VALUES(%s, %s, %s)", user, name, value)
+			else:
+			    self.application.db.execute(
+				"UPDATE entries SET name = %s, value = %s"
+				"WHERE user = %s", name, value, user) 
+
+	def post(self):
+		print "haha"
+
+
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == "__main__":
+    main()
