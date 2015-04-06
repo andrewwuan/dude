60da91a9057975161f0f7cb6abb84f37ec7744cb
An Wu <anwu@andrew.cmu.edu>
2015-04-06 08:57:11 -0400

    retab to spaces

--- 549project/server.py
+++ 549project/server.py
@@ -22,40 +22,40 @@ define("mysql_user", default="dude", help="dude database user")
 define("mysql_password", default="dude", help="dude database password")
 
 class Application(tornado.web.Application):
-	def __init__(self):
-		handlers = [
-			(r"/", MainHandler),
-		]
-		tornado.web.Application.__init__(self, handlers)
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+        ]
+        tornado.web.Application.__init__(self, handlers)
 
-		# Have one global connection to the blog DB across all handlers
-		self.db = torndb.Connection(
-			host=options.mysql_host, database=options.mysql_database,
-			user=options.mysql_user, password=options.mysql_password)
+        # Have one global connection to the blog DB across all handlers
+        self.db = torndb.Connection(
+            host=options.mysql_host, database=options.mysql_database,
+            user=options.mysql_user, password=options.mysql_password)
 
 
 class MainHandler(tornado.web.RequestHandler):
-	def get(self):
-		user = self.get_argument("user")
-		name = self.get_argument("name")
-		value = self.get_argument("value", None)
-		if (not value):
-			entry = self.application.db.get("SELECT * FROM entries WHERE user = %s AND name = %s",
-				user, name)
-			self.write(entry.value)
-		else:
-			entry = self.application.db.get("SELECT * FROM entries WHERE user = %s", user)
-			if not entry:
-				self.application.db.execute(
-				"INSERT INTO entries VALUES(%s, %s, %s)", user, name, value)
-			else:
-			    self.application.db.execute(
-				"UPDATE entries SET name = %s, value = %s"
-				"WHERE user = %s", name, value, user) 
+    def get(self):
+        user = self.get_argument("user")
+        name = self.get_argument("name")
+        value = self.get_argument("value", None)
+        if (not value):
+            entry = self.application.db.get("SELECT * FROM entries WHERE user = %s AND name = %s",
+                user, name)
+            self.write(entry.value)
+        else:
+            entry = self.application.db.get("SELECT * FROM entries WHERE user = %s", user)
+            if not entry:
+                self.application.db.execute(
+                "INSERT INTO entries VALUES(%s, %s, %s)", user, name, value)
+            else:
+                self.application.db.execute(
+                "UPDATE entries SET name = %s, value = %s"
+                "WHERE user = %s", name, value, user) 
 
-	def post(self):
-		f = open("wavfile.wav", "wb")
-		f.write(self.request.body)
+    def post(self):
+        f = open("wavfile.wav", "wb")
+        f.write(self.request.body)
 
 
 def main():
