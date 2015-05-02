b072179488b6273ad4c71629241ba74dc8cda939
An Wu <anwu@andrew.cmu.edu>
2015-05-02 17:05:17 -0400

    fix message

--- 549project/main.py
+++ 549project/main.py
@@ -55,9 +55,9 @@ while (True):
         if (user != ''):
             packet = check_message(user, options.host, options.port)
             if (len(packet) != 0):
-                message = "%s, you have a message from %s." % (user, packet[0])
+                message = "%s, you have a message from %s." % (user, packet[0]['user'])
                 subprocess.call(["./text2speech.sh", message])
-                subprocess.call(["./text2speech.sh", packet[1]])
+                subprocess.call(["./text2speech.sh", packet[0]['message']])
 
         # Post temperature & brightness data
         set_temperature(options.device, readTemperature(), options.host, options.port)
