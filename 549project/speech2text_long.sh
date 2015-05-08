e3300db786f857cfaa354b7ab1570e6f50fbc261
An Wu <anwu@andrew.cmu.edu>
2015-05-08 00:02:36 -0400

    change key

--- 549project/speech2text_long.sh
+++ 549project/speech2text_long.sh
@@ -3,10 +3,10 @@
 #echo "Recording..."
 arecord -D "plughw:1,0" -d 4 -q -f cd -t wav -r 16000 | flac - -f --best --sample-rate 16000 -s -o request.flac
 #echo "Uploading to Google..."
-#wget -q -U "Mozilla/5.0" --post-file request.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v2/recognize?lang=en-us&client=chromium&key=AIzaSyCCJMVMpF-prxcR9_vCoyhbVUsNjAqMw4w" | cut -d\" -f8 > stt.txt
+wget -q -U "Mozilla/5.0" --post-file request.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v2/recognize?lang=en-us&client=chromium&key=AIzaSyCCJMVMpF-prxcR9_vCoyhbVUsNjAqMw4w" | cut -d\" -f8 > stt.txt
 #wget -q -U "Mozilla/5.0" --post-file request.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v2/recognize?lang=en-us&client=chromium&key=AIzaSyCNtR-Gkh4uhob8J1oxy6MtpyNn_FNs7vg" | cut -d\" -f8 > stt.txt
 #wget -q -U "Mozilla/5.0" --post-file request.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v2/recognize?lang=en-us&client=chromium&key=AIzaSyBhDRjJDmZ23ZvYPni8--O3dxUauAZKYvs" | cut -d\" -f8 > stt.txt
 #wget -q -U "Mozilla/5.0" --post-file request.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v2/recognize?lang=en-us&client=chromium&key=AIzaSyAY7G3fRXOVODejFit7isph3OOvY-bmJPI" | cut -d\" -f8 > stt.txt
-wget -q -U "Mozilla/5.0" --post-file request.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v2/recognize?lang=en-us&client=chromium&key=AIzaSyB9p1nB1JodqUUHWuhBs7ls3RmPYEjsDSE" | cut -d\" -f8 > stt.txt
+#wget -q -U "Mozilla/5.0" --post-file request.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v2/recognize?lang=en-us&client=chromium&key=AIzaSyB9p1nB1JodqUUHWuhBs7ls3RmPYEjsDSE" | cut -d\" -f8 > stt.txt
 #echo "You just said: $(cat stt.txt)"
 
--- 549project/speech2text_short.sh
+++ 549project/speech2text_short.sh
@@ -4,10 +4,10 @@
 arecord -D "plughw:1,0" -d 2 -q -f cd -t wav -r 16000 -c 1 dude.wav
 flac dude.wav -f --best --sample-rate 16000 -s -o dude.flac
 #echo "Uploading to Google..."
-#wget -q -U "Mozilla/5.0" --post-file dude.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v2/recognize?lang=en-us&client=chromium&key=AIzaSyCCJMVMpF-prxcR9_vCoyhbVUsNjAqMw4w" | cut -d\" -f8 > stt.txt
+wget -q -U "Mozilla/5.0" --post-file dude.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v2/recognize?lang=en-us&client=chromium&key=AIzaSyCCJMVMpF-prxcR9_vCoyhbVUsNjAqMw4w" | cut -d\" -f8 > stt.txt
 #wget -q -U "Mozilla/5.0" --post-file dude.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v2/recognize?lang=en-us&client=chromium&key=AIzaSyCNtR-Gkh4uhob8J1oxy6MtpyNn_FNs7vg" | cut -d\" -f8 > stt.txt
 #wget -q -U "Mozilla/5.0" --post-file dude.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v2/recognize?lang=en-us&client=chromium&key=AIzaSyBhDRjJDmZ23ZvYPni8--O3dxUauAZKYvs" | cut -d\" -f8 > stt.txt
 #wget -q -U "Mozilla/5.0" --post-file dude.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v2/recognize?lang=en-us&client=chromium&key=AIzaSyAY7G3fRXOVODejFit7isph3OOvY-bmJPI" | cut -d\" -f8 > stt.txt
-wget -q -U "Mozilla/5.0" --post-file dude.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v2/recognize?lang=en-us&client=chromium&key=AIzaSyB9p1nB1JodqUUHWuhBs7ls3RmPYEjsDSE" | cut -d\" -f8 > stt.txt
+#wget -q -U "Mozilla/5.0" --post-file dude.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v2/recognize?lang=en-us&client=chromium&key=AIzaSyB9p1nB1JodqUUHWuhBs7ls3RmPYEjsDSE" | cut -d\" -f8 > stt.txt
 #echo "You just said: $(cat stt.txt)"
 
